98d0bb17704644a8042041a33d72fb1ff967367b
vankhanh02 <khanhvvan202@gmail.com>
2024-05-24 23:01:20 +0700

    add item

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
-const Item = () => {
+import './Item.css'
+const Item = (props) => {
   return (
-    <div>Item</div>
+    <div className="item">
+        <img src={props.image} alt="image" />
+        <p>{props.name}</p>
+        <div className="item-price">
+            <div className="price-new">
+                {props.new_price}
+            </div>
+            <div className="price-old">
+                {props.old_price}
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
---
+++ frontend/src/Pages/Authenication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Authenication = () => {
+  return (
+    <div>Authenication</div>
+  )
+}
+
+export default Authenication
