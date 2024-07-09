27952df9e550b6a0778791034821ef105de8207a
vankhanh02 <khanhvvan202@gmail.com>
2024-07-10 00:28:08 +0700

    add post api

--- backend/index.js
+++ backend/index.js
@@ -18,23 +18,37 @@ mongoose.connect("mongodb+srv://khanhvvan:060102@cluster0.hofsmry.mongodb.net/e-
 app.get("/", (req, res) =>{
     res.send("Express App is running")
 
-})
-app.listen(port, (error) => {
-    if (!error){
-        console.log("Server is running on Port " + port)
-    }
-    else{
-        console.log("Error : " + error)
-    }
-
-})
+});
 
 //Image storage
 
 const storage = multer.diskStorage({
     //pass object
-    destination: './upload/image',
+    destination: './upload/images',
     filename: (req, file, cb) =>{
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
-})
+});
+
+const upload = multer({storage:storage});
+//upload endpoint
+app.use('/images', express.static('upload/images'));
+
+app.post("upload", upload.single('product'), (req,res) =>{
+    res.json({
+        success:1, 
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+
+app.listen(port, (error) => {
+    if (!error){
+        console.log("Server is running on Port " + port);
+    }
+    else{
+        console.log("Error : " + error);
+    }
+
+});
+
