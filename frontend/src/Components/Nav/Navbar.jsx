6655c5123cd2a1a05a62ad4a5c2e9dee2c548821
vankhanh02 <khanhvvan202@gmail.com>
2024-07-09 11:19:46 +0700

    adjust item layout

--- frontend/src/Components/Nav/Navbar.jsx
+++ frontend/src/Components/Nav/Navbar.jsx
@@ -5,6 +5,7 @@ import logo from '../Assets/store.png'
 import cart_icon from '../Assets/shopping-bag.png'
 import { ShopContext } from '../../Context/ShopContext'
 import dropdown from '../Assets/dropdown_icon.png'
+import nav_dropdown from '../Assets/down-arrow.png'
 
 export const Navbar = (e) => {
   const [menu,setMenu] = useState("shop")
@@ -20,24 +21,24 @@ export const Navbar = (e) => {
         <img src={logo} alt="Logo" />
         <p>SHOPPER</p>
       </div>
-      <img className= "nav-dropdown" onClick={dropdown_toggle} src={dropdown} alt="" />
+      <img className= "nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
-          <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop </Link>{menu==="shop"?< hr/>: <></>} 
-          </li>
-          <li onClick={()=>{setMenu("men")}}><Link to='/mens'>Men </Link> {menu==="men"?< hr/>: <></>}
-          </li>
-          <li onClick={()=>{setMenu("women")}}><Link to='/womens'>Women </Link> {menu==="women"?< hr/>: <></>} 
-          </li>
-          <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kid </Link> {menu==="kids"?< hr/>: <></>} 
-          </li>
-          <li onClick={()=>{setMenu("about")}}><Link to='/about'>About </Link> {menu==="about"?< hr/>: <></>} 
-          </li>
-        </ul>
-        <div className="nav-login">
-          <Link to ="/login"><button>Login</button></Link>
-          <Link to = "/cart"><img src={cart_icon}  alt="cart" /></Link>
-          <div className="nav-cart-count">{getTotalCartItem()}</div>
-        </div>
+        <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop </Link>{menu==="shop"?< hr/>: <></>} 
+        </li>
+        <li onClick={()=>{setMenu("men")}}><Link to='/mens'>Men </Link> {menu==="men"?< hr/>: <></>}
+        </li>
+        <li onClick={()=>{setMenu("women")}}><Link to='/womens'>Women </Link> {menu==="women"?< hr/>: <></>} 
+        </li>
+        <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kid </Link> {menu==="kids"?< hr/>: <></>} 
+        </li>
+        <li onClick={()=>{setMenu("about")}}><Link to='/about'>About </Link> {menu==="about"?< hr/>: <></>} 
+        </li>
+      </ul>
+      <div className="nav-login">
+        <Link to ="/login"><button>Login</button></Link>
+        <Link to = "/cart"><img src={cart_icon}  alt="cart" /></Link>
+        <div className="nav-cart-count">{getTotalCartItem()}</div>
+      </div>
     </div>
   )
 }
