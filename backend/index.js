53dd75aab39c8f570ef8b2d21e958cc93b81cc72
vankhanh02 <khanhvvan202@gmail.com>
2024-07-11 23:54:44 +0700

    product api

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ const upload = multer({storage:storage});
 
 app.use('/images', express.static('upload/images'));
 
-app.post("upload", upload.single('product'), (req,res) =>{
+app.post("/upload", upload.single('product'), (req,res) =>{
     res.json({
         success:1, 
         image_url:`http://localhost:${port}/images/${req.file.filename}`
@@ -80,9 +80,19 @@ const Product= mongoose.model("Product", {
     },
 })
 
-app.post('/addproduct', async (res, req) =>{
+app.post('/addproduct', async (req, res) =>{
+    let products = await Product.find({});
+    let id;
+    if (products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id= last_product.id +1
+    }
+    else {
+        id=1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
@@ -97,6 +107,24 @@ app.post('/addproduct', async (res, req) =>{
         name: req.body.name,
     })
 } )
+
+// delete product API
+app.post('/removeproduct', async (req, res) =>{
+    await Product.findOneAndDelete({id: req.body.id});
+    console.log("Removed");
+    res.json({
+        success:1,
+        name: req.body.name
+    })
+})
+
+//get all product
+app.get('/allproduct', async(req,res)=>{
+    let products = await Product.find({})
+    console.log("All Product Fetched");
+    res.send(products);
+
+})
 app.listen(port, (error) => {
     if (!error){
         console.log("Server is running on Port " + port);
