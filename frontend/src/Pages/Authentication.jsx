8be716987e4fd14d0895f8978c7cd8db1d21b06c
vankhanh02 <khanhvvan202@gmail.com>
2024-07-18 00:43:08 +0700

    add login

--- frontend/src/Pages/Authentication.jsx
+++ frontend/src/Pages/Authentication.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Authen.css'
 const Authentication = () => {
+
+  const [state, setState] = useState("Login");
+  
+  const login = async () =>{
+    console.log("Login Function Exe")
+    
+  }
+  const signup = async () =>{
+    console.log("Sign Up Function Exe")
+  }
+
   return (
     <div className="authen">
       <div className="container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="field">
-          <input type="text" placeholder='Your Name' />
+          {state==="Sign Up"?<input type="text" placeholder='Your Name' />: <></>}
           <input type="email" placeholder='Your Email' />
           <input type="password" placeholder='Password' />
         </div>
-        <button>Continue</button>
-        <p className="loginmess">
+        <button onClick={ ()=> { state==="Login"? login():signup()}}>Continue</button>
+        {state==="Sign Up"?
+          <p className="loginmess">
           Already have an account? 
-          <span>Login</span>
+          <span onClick={ ()=> {setState("Login")}}> Login</span>
+        </p>:
+          <p className="loginmess">
+          Create an account? 
+          <span onClick={()=>{setState("Sign Up")}}> Click here</span>
         </p>
+
+        }
         <div className="signupagree">
           <input type="checkbox" name='' id=''/>
           <p>By continuing, I agree to the terms of use and privacy policy</p>
