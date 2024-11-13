1306ddd60e97477601eea325c03abb053ec99589
vankhanh02 <khanhvvan202@gmail.com>
2024-11-13 13:25:21 +0700

    fix ad

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navprofile from '../../assets/nav-profile.svg'
+import React from "react";
+import "./Navbar.css";
+import navlogo from "../../assets/nav-logo.svg";
+import navprofile from "../../assets/nav-profile.svg";
+import logo from "../../assets/store.png";
 const Navbar = () => {
   return (
     <div className="navbar">
-        <img src={navlogo} alt=""  className='nav-logo'/>
-        <img src={navprofile} alt="" className="nav-pro" />
+      <div className="logo">
+        <img src={logo} alt="" className="nav-logo" />
+        <p> SHOPPER</p>
+      </div>
+      <img src={navprofile} alt="" className="nav-pro" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
