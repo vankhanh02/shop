9d3245e5da0c0bb9a936b4a10efa2ebcf7919556
vankhanh02 <khanhvvan202@gmail.com>
2024-11-20 21:54:27 +0700

    edit env

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
-const port = 4000;
 // Import dependencies using ES module syntax
 import express from "express";
-import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
 import multer from "multer";
 import path from "path";
 import cors from "cors";
-import { StringDecoder } from "string_decoder";
 import productRouter from "./routes/productRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
-import userModel from "./models/userModel.js";
-import productModel from "./models/productModel.js";
 import { dbConnection } from "./config/dataconfig.js";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT;
+const baseURL = process.env.BASE_URL;
+
 app.use(express.json());
 app.use(cors());
 
@@ -47,28 +47,12 @@ app.use("/images", express.static(path.join(__dirname, "upload/images")));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `${baseURL}:${port}/images/${req.file.filename}`,
   });
 });
 app.use("/api/product", productRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-
-/* // create new collection
-
-app.get("/newcollection", async (req, res) => {
-  let products = await productModel.find({});
-  let newcollection = products.slice(1).slice(-8);
-  console.log("New Collection Fetch");
-  res.send(newcollection);
-});
-// popular in woman
-app.get("/popularwomen", async (req, res) => {
-  let products = await productModel.find({ category: "women" });
-  let popularwomen = products.slice(0, 4);
-  console.log("Popular in women fetch");
-  res.send(popularwomen);
-}); */
 
 app.listen(port, (error) => {
   if (!error) {
